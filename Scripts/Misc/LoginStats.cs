204dcd5912ded42d016aadf8140af849a7c98dcb
Cork <jmachie@hotmail.com>
2026-04-05 15:58:41 -0500

    Feature: GMToolbar AutoStart

--- Scripts/Misc/LoginStats.cs
+++ Scripts/Misc/LoginStats.cs
@@ -1,6 +1,7 @@
 using System;
 using Server.Network;
 using Server.Engines.Quests;
+using Server.Gumps;
 using Server.Mobiles;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,6 +34,7 @@ namespace Server.Misc
             if (m.IsStaff())
             {
                 Server.Engines.Help.PageQueue.Pages_OnCalled(m);
+                m.SendGump(new GMToolbarGump(m, false));
             }
         }
     }
