be07491396a9e2d0450c64fa8e00d0b439e42ad3
Cork <jmachie@hotmail.com>
2026-04-10 16:35:50 -0500

    Reduced range to 3 tiles

--- Scripts/Spells/Base/SpellHelper.cs
+++ Scripts/Spells/Base/SpellHelper.cs
@@ -1233,7 +1233,7 @@ namespace Server.Spells
 
         public static bool CheckFieldWard(Point3D loc, Map map, Mobile caster)
         {
-            IPooledEnumerable eable = map.GetItemsInRange(loc, 4);
+            IPooledEnumerable eable = map.GetItemsInRange(loc, 3);
 
             foreach (Item item in eable)
             {
