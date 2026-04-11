369d58af304a3f81d4e0f70ee6bb07396e430c8e
Cork <jmachie@hotmail.com>
2026-04-11 16:44:25 -0500

    Changed gump graphic to include transparancy.

--- Scripts/Custom/NPC/RepairNPC/RepairNpcAll.cs
+++ Scripts/Custom/NPC/RepairNPC/RepairNpcAll.cs
@@ -60,7 +60,8 @@ namespace Server.Gumps
             Resizable = false;
 
             AddPage(0);
-            AddBackground(0, 0, 760, 600, 5054);
+            AddBackground(0, 0, 760, 600, 9270);
+            AddAlphaRegion(10, 10, 740, 580);
             AddLabel(270, 20, 37, @"Equipment Repair Service");
 
             CollectEquipmentInfo();
