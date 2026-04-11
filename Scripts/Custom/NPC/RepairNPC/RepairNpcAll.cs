3f9142e65edc3fb34c5470c08521212798552348
Cork <jmachie@hotmail.com>
2026-04-11 16:46:47 -0500

    Adjusting the AddAlphaRegion for transparancy.

--- Scripts/Custom/NPC/RepairNPC/RepairNpcAll.cs
+++ Scripts/Custom/NPC/RepairNPC/RepairNpcAll.cs
@@ -61,7 +61,7 @@ namespace Server.Gumps
 
             AddPage(0);
             AddBackground(0, 0, 760, 600, 9270);
-            AddAlphaRegion(10, 10, 740, 580);
+            AddAlphaRegion(0, 0, 760, 600);
             AddLabel(270, 20, 37, @"Equipment Repair Service");
 
             CollectEquipmentInfo();
