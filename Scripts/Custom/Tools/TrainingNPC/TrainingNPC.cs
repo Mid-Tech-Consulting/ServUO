09c34127445b222a925f523cbfa27c1a9508a078
Cork <jmachie@hotmail.com>
2026-04-12 16:02:18 -0500

    Changed the chance for a stat gain from 50% to 35%.

--- Scripts/Custom/Tools/TrainingNPC/TrainingNPC.cs
+++ Scripts/Custom/Tools/TrainingNPC/TrainingNPC.cs
@@ -82,8 +82,8 @@ namespace Server.Mobiles
             if (!(attacker is PlayerMobile player))
                 return;
 
-            // 50% chance per hit to attempt a stat gain
-            if (Utility.Random(100) >= 50)
+            // 35% chance per hit to attempt a stat gain
+            if (Utility.Random(100) >= 35)
                 return;
 
             // Pick which stat to try based on what the player has unlocked (set to Up)
