ff8dc2f78218336c2d1ada8f85e13649fca8900a
Cork <jmachie@hotmail.com>
2026-04-26 14:18:01 -0500

    Request: Change hailstorm to do instant instead of delayed damage.
    Fix: Change DelayedDamage from true to false.

--- Scripts/Spells/Mysticism/SpellDefinitions/HailStormSpell.cs
+++ Scripts/Spells/Mysticism/SpellDefinitions/HailStormSpell.cs
@@ -13,7 +13,7 @@ namespace Server.Spells.Mysticism
     public class HailStormSpell : MysticSpell
     {
         public override SpellCircle Circle { get { return SpellCircle.Seventh; } }
-        public override bool DelayedDamage { get { return true; } }
+        public override bool DelayedDamage { get { return false; } }
         public override DamageType SpellDamageType { get { return DamageType.SpellAOE; } }
 
         private static SpellInfo m_Info = new SpellInfo(
