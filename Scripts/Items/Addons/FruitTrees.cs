3f27b8c4b2811bac454b041f36bd3483ffea408b
Cork <jmachie@hotmail.com>
2026-05-05 11:56:18 -0500

    Cleanup: Moved the plum tree class in to the fruittrees.cs file so all fruit trees exist in the same place. Then deleted the separate plumtreeaddon.cs file.

--- Scripts/Items/Addons/FruitTrees.cs
+++ Scripts/Items/Addons/FruitTrees.cs
@@ -264,4 +264,68 @@ namespace Server.Items
             int version = reader.ReadEncodedInt();
         }
     }
+
+    public class PlumTreeAddon : BaseFruitTreeAddon
+    {
+        [Constructable]
+        public PlumTreeAddon()
+            : base()
+        {
+            AddComponent(new LocalizedAddonComponent(0x9E38, 1029965), 0, 0, 0);
+            AddComponent(new LocalizedAddonComponent(0x9E39, 1029965), 0, 0, 0);
+        }
+
+        public PlumTreeAddon(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override BaseAddonDeed Deed { get { return new PlumTreeAddonDeed(); } }
+        public override Item Fruit { get { return new Plum(); } }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+    }
+
+    public class PlumTreeAddonDeed : BaseAddonDeed
+    {
+        [Constructable]
+        public PlumTreeAddonDeed()
+            : base()
+        {
+        }
+
+        public PlumTreeAddonDeed(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override int LabelNumber { get { return 1157312; } } // Plum Tree
+        public override BaseAddon Addon { get { return new PlumTreeAddon(); } }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+    }
 }
